043f7fed19b242c5da58d925c09822ab41a66659
italo.nascimento <italo.leo.14@gmail.com>
2019-06-16 01:38:48 -0300

    ws

--- index.js
+++ index.js
@@ -4,10 +4,28 @@ const cheerio = require('cheerio')
 const reques = require('request')
 const PORT = process.env.PORT || 5000
 var a = "hell";
+
+var Crawler = require("crawler").Crawler;
+var c = new Crawler({
+ "maxConnections":10,
+ 
+ // This will be called for each crawled page
+ "callback":function(error,result,$) {
+ var title = $("h1").text();
+ 
+ }
+});
+// Queue just one URL, with default callback
+c.queue("http://www.tudogostoso.com.br/receita/168837-bolo-de-mucilon.html");
+
+
+
+
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   //.get('/', (req, res) => res.render('pages/index'))
-  .get('/', (req, res) => res.send(a))
+  .get('/', (req, res) => res.send(title))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
