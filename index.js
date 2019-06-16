777ece8b987d28698b7e7764a3369fd3a8c535cc
italo.nascimento <italo.leo.14@gmail.com>
2019-06-16 00:36:02 -0300

    ws

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
 const cheerio = require('cheerio')
+const reques = require('request')
 const PORT = process.env.PORT || 5000
 
 express()
