ac8619899aa48be91cb6e019fea86f948ab3c8e6
italo.nascimento <italo.leo.14@gmail.com>
2019-06-16 01:48:46 -0300

    ws

--- index.js
+++ index.js
@@ -2,19 +2,27 @@ const express = require('express')
 const path = require('path')
 const cheerio = require('cheerio')
 const reques = require('request')
-const PORT = process.env.PORT || 5000
-var a = "hell";
+var Crawler = require('crawler')
 
-var Crawler = require("crawler").Crawler;
+const PORT = process.env.PORT || 5000
+var title = "";
+
 var c = new Crawler({
- "maxConnections":10,
- 
- // This will be called for each crawled page
- "callback":function(error,result,$) {
- var title = $("h1").text();
- 
- }
+    maxConnections : 10,
+    // This will be called for each crawled page
+    callback : function (error, res, done) {
+        if(error){
+            console.log(error);
+        }else{
+            var $ = res.$;
+            // $ is Cheerio by default
+            //a lean implementation of core jQuery designed specifically for the server
+          title =  console.log($("title").text());
+        }
+        done();
+    }
 });
+ 
 // Queue just one URL, with default callback
 c.queue("http://www.tudogostoso.com.br/receita/168837-bolo-de-mucilon.html");
 
