503f714de95beee5e03aea701fc545c8b47d665d
italo.nascimento <italo.leo.14@gmail.com>
2019-06-15 23:52:05 -0300

    web croowler

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const path = require('path')
+const path = require('cheerio')
+const path = require('request')
+
 const PORT = process.env.PORT || 5000
 
 express()
