7fc00beee64a8e33195ed6f7f5a19beeb79ce44b
italo.nascimento <italo.leo.14@gmail.com>
2019-06-16 01:07:45 -0300

    aa

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const path = require('path')
 const cheerio = require('cheerio')
 const reques = require('request')
 const PORT = process.env.PORT || 5000
-
+var a = "hell";
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   //.get('/', (req, res) => res.render('pages/index'))
-  .get('/', (req, res) => res.send('Hello World!'))
+  .get('/', (req, res) => res.send(a))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
