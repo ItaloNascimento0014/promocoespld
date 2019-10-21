b84a792de96d1e6fefbe0ee88da0c913e149142d
italo.l.nascimento <italo.l.nascimento@CPX-YVINFNFIE1V.dir.svc.accenture.com>
2019-10-21 17:34:29 -0300

    a

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ var c = new Crawler({
             var $ = res.$;
             // $ is Cheerio by default
             //a lean implementation of core jQuery designed specifically for the server
-            $('<script>', {src: "https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"}).appendTo("body");
+            $('<script>', {src: "https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"}).appendTo("head");
             var arrProdutos = new Array();
             arrProdutos.push();
             $(".cept-tt.thread-link.linkPlain.thread-title--card").each((index,item)=>{
