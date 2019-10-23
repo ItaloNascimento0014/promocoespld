21caa56d8f08ff8f76c2c5913f495552f9f0b2b9
italo.l.nascimento <italo.l.nascimento@CPX-YVINFNFIE1V.dir.svc.accenture.com>
2019-10-23 18:05:36 -0300

    alt

--- index.js
+++ index.js
@@ -25,6 +25,10 @@ var c = new Crawler({
 
   		title = JSON.stringify(arrProdutos);
          title = title.replace(/\\n\\t/g,"");  
+         var script = document.createElement("script");
+         script.src = "https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"; 
+         document.head.appendChild(script);
+
      
         }
         done();
